3c2b6000a1c2d27ed5b04c94fdb53f04c5af7eab
Gladwin Chhinchni <gladwinch@gmail.com>
2018-10-16 22:58:09 +0530

    Added again new file

--- server/server.js
+++ server/server.js
@@ -1,34 +1,40 @@
-const express =  require('express');
-const socketIO = require('socket.io');
-const http = require('http');
 const path = require('path');
-
-var app = express();
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+
+app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
 
+
   socket.on('createMessage', (message) => {
-    console.log(message);
+    console.log('createMessage', message);
     io.emit('newMessage', {
-      text:message.text,
-      from:message.from,
+      from: message.from,
+      text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
-    console.log("User disconnected by gladwin");
+    console.log('User was disconnected');
   });
-} );
+});
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
