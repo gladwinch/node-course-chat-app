75021665bf7afbbbba50856ca5756347fa7caf84
Gladwin Chhinchni <gladwinch@gmail.com>
2018-10-17 07:30:01 +0530

    Add new user

--- server/server.js
+++ server/server.js
@@ -14,20 +14,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit("newMessage", {greeting: "Welcome to chat app"});
+  socket.broadcast.emit('newMessage', {User: "New user joined"})
 
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // });
+
   });
 
   socket.on('disconnect', () => {
